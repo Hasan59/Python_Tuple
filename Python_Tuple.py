54f28b254da74b627da397700f9e55ed7ce5a5ec
Hasan Mahmud <1059hasan@gmail.com>
2022-06-14 22:43:02 +0600

    2nd commit

--- Python_Tuple.py
+++ Python_Tuple.py
@@ -3,3 +3,28 @@ print(thistuple)
 
 thistuple = ("apple", "banana", "cherry", "apple", "cherry")
 print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+thistuple = ("apple")
+print(type(thistuple))
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
